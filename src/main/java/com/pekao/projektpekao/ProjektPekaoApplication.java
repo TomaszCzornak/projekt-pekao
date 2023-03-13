f9dc3cdfb8e426d79fba40b0b1364ea79a6357d2
TomaszCzornak <tomasz.czornak@outlook.com>
2023-03-13 12:49:43 +0100

    seeding data into DB

--- src/main/java/com/pekao/projektpekao/ProjektPekaoApplication.java
+++ src/main/java/com/pekao/projektpekao/ProjektPekaoApplication.java
@@ -1,11 +1,14 @@
 package com.pekao.projektpekao;
 
+import com.pekao.projektpekao.entity.Author;
 import com.pekao.projektpekao.entity.Book;
 import com.pekao.projektpekao.repository.AuthorRepository;
 import com.pekao.projektpekao.repository.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ProjektPekaoApplication implements CommandLineRunner {
@@ -24,7 +27,13 @@ public class ProjektPekaoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Author author1 = new Author("John", "Walker");
+        Author author2 = new Author("Brian", "May");
+        authorRepository.saveAll(Arrays.asList(author1, author2));
 
-
+        Book book1 = new Book("Spring w Akcji", author1);
+        Book book2 = new Book("Java", author2);
+        Book book3 = new Book("Programowanie funkcyjne", author2);
+        bookRepository.saveAll(Arrays.asList(book1, book2, book3));
     }
 }
