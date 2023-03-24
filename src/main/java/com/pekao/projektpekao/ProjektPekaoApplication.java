bff47d3e00c8171869ae9b89c9c98963b2f25d53
TomaszCzornak <tomasz.czornak@outlook.com>
2023-03-24 11:48:06 +0100

    entity recursion resolved

--- src/main/java/com/pekao/projektpekao/ProjektPekaoApplication.java
+++ src/main/java/com/pekao/projektpekao/ProjektPekaoApplication.java
@@ -36,28 +36,31 @@ public class ProjektPekaoApplication implements CommandLineRunner {
         SpringApplication.run(ProjektPekaoApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-//        Author author1 = new Author("Craig", "Walls");
-//        Author author2 = new Author("Pierre-Yves", "Saumont");
-//
-//        authorRepository.saveAll(Arrays.asList(author1, author2));
-//
-//
-//        Book book1 = new Book("Spring w Akcji", author1, List.of(comment2));
-//        Book book2 = new Book("Java", author2, List.of(comment1));
-//        Book book3 = new Book("Programowanie funkcyjne", author2, List.of(comment3));
-//        bookRepository.saveAll(Arrays.asList(book1, book2, book3));
-//
-//        User user1 = new User("Tomek", "Czornak", "tomek@gmail.com", new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z")
-//                .format(new Date()), List.of(comment3));
-//        User user2 = new User("Marek", "Nowakowski", "mareknowakowski@gmail.com", new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z")
-//                .format(new Date()), List.of(comment1, comment2));
-//        userRepository.saveAll(List.of(user1,user2));
-//        Comment comment1 = new Comment("Dupa Jasiu");
-//        Comment comment2 = new Comment("Kolejny komentarz");
-//        Comment comment3= new Comment("Ta książka jest ok");
-//        commentRepository.saveAll(Arrays.asList(comment1, comment2, comment3));
 
+        @Override
+        public void run (String...args){
+            Author author1 = new Author("Craig", "Walls");
+            Author author2 = new Author("Pierre-Yves", "Saumont");
+
+            authorRepository.saveAll(List.of(author1, author2));
+
+            Comment comment1 = commentRepository.save(new Comment("Dupa Jasiu"));
+            Comment comment2 = commentRepository.save(new Comment("Kolejny komentarz"));
+            Comment comment3 = commentRepository.save(new Comment("Ta książka jest ok"));
+
+            User user1 = new User("Tomek", "Czornak", "tomek@gmail.com", new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z")
+                    .format(new Date()), List.of(comment3));
+            User user2 = new User("Marek", "Nowakowski", "mareknowakowski@gmail.com", new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z")
+                    .format(new Date()), List.of(comment1, comment2));
+            userRepository.saveAll(List.of(user1, user2));
+            commentRepository.saveAll(List.of(comment1, comment2, comment3));
+
+            Book book1 = new Book("Spring w Akcji", author1, List.of(comment2));
+            Book book2 = new Book("Java", author2, List.of(comment1));
+            Book book3 = new Book("Programowanie funkcyjne", author2, List.of(comment3));
+            bookRepository.saveAll(List.of(book1, book2, book3));
+            commentRepository.saveAll(List.of(comment1, comment2, comment3));
+
+        }
     }
 }
